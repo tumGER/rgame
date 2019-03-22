6a8ce1e560bb6bc953942526a092948636cbdcaa
tomGER <25822956+tumGER@users.noreply.github.com>
2019-03-22 16:33:02 +0100

    Update rgame.py

--- rgame/rgame.py
+++ rgame/rgame.py
@@ -303,8 +303,8 @@ class rgame:
         await self.bot.say("https://cdn.discordapp.com/attachments/365038956783599617/367011950858731541/Oshit.mp3")
 
     @commands.command()
-    async def levelepsilon(self):
-        """Clue Level epsilon"""
+    async def levela(self):
+        """Clue LevelA"""
 
         await self.bot.say("A hint once hated now your best friend :) \n ```sudo ❏```")    
         
