9d4177ce9b7a4e0276ca87fab501bff6c01717cc
Andrew Grindstaff <ajgrinds03@gmail.com>
2020-08-09 23:31:22 -0700

    Changed it to use a dictionary and 1 command

--- rgame/rgame.py
+++ rgame/rgame.py
@@ -12,307 +12,78 @@ class rgame:
     def __init__(self, bot):
         self.bot = bot
         self.client = discord.Client()
-        
-    @commands.command()
-    async def level1(self):
-        """Clue Level 1"""
+        self.bot.clues = {
+            "1": "The clue is in the description of /r/gamelevel2, which is Who?",
+            "2": "The clue is in the description of /r/gamelevel3. | What are you supposed to do on Reddit?",
+            "3": "The clue is in the description of /r/gamelevel4. The clue is We are PRO CSS! | Use old.reddit.com",
+            "4": "Make sure you have a QWERTY keyboard",
+            "5": "Pay close attention to the Reddit sidebar, and remember: two eyes.",
+            "6": "The clue is in the sidebar. It is mod vanity. | a spoiler tag in the post. Super major clue",
+            "7": "A site set up specifically for this level... hosted by the burning heavens.",
+            "8": "The most special things are out of reach sometimes.",
+            "9": "Clue is in the title. I can't see you. | But first let ...........",
+            "10": "Clue is the header image. There are also clues in the sidebar and blatantly in CSS.",
+            "11": "While the effect is best if you have RES, you could rely on a previous trick to find the answer.",
+            "12": "It's Shane is the clue",
+            "13": "So far you've been advancing through time... Now you need to roll it back to 0.",
+            "x": "It's a Übchi. The keyword is game.",
+            "14": "Review the first few levels. You may find there's a level you haven't been to yet.",
+            "15": "The date is one number off.",
+            "xx": "The clue is in the description of level 15.",
+            "16": "53 6f 20 79 6f 75 20 74 68 6f 75 67 68 74 20 74 68 61 74 20 79 6f 75 20 77 6f 75 6c 64 20 66 69 6e "
+                  "64 20 74 68 65 20 61 6e 73 77 65 72 20 62 79 20 64 65 63 6f 64 69 6e 67 20 74 68 69 73 20 74 65 78 "
+                  "74 20 62 75 74 20 74 68 61 74 20 69 73 20 62 75 6c 6c 73 68 69 74 2c 20 67 6f 20 62 61 63 6b 20 74 "
+                  "6f 20 74 68 65 20 73 75 62 72 65 64 64 69 74 20 61 6e 64 20 6c 6f 6f 6b 20 44 45 45 50 20 69 6e 74 "
+                  "6f 20 74 68 65 20 70 69 63 74 75 72 65 ",
+            "17": "Why might somebody use a program with an onion icon?",
+            "18": "hidden spoiler",
+            "19": "Secret Sauce",
+            "xxx": "A gif is stickied - Look at it :thinking:",
+            "20": "The clue is, Hard to h**ear** with a picture of an empty vase.",
+            "21": "I soametims do typos :thinking:",
+            # is this misspelling purposeful?
+            "22": "One way to solve is to find /r/FutureGaming",
+            "23": "The clue is, who am I (zadoc) not?",
+            "24": "The clue is american wow.",
+            "25": "The clue is a reference to our Discord. Just ask the bot for the answer!",
+            "26": "The clue is a set of multicolored [!] in the sidebar",
+            "27": "The clue is, I cant believe we never used this one before. Also, in the description it says, naw.",
+            "272": "There are four RUE's, all the E's are red, and there's a pair of dice.",
+            "28": "Clue = Wat dis?",
+            "29": "The clue is: X - α4 = Y. Solve for Y. I give them that according to Zadoc",
+            "30": "What happened in 1997?",
+            "31": "Give no hints except 1, is our order! Sorry ;P",
+            "32": "Do the things my creator doesnt do often",
+            "33": "They have to decode the sound",
+            "34": "90's kids will remember text like this",
+            "35": "Ask a mod for hints to the numbers, this one is impossible for to answer as a bot - Be angry at Tom "
+                  "for not giving me AI",
+            "36": "Idk, ask Zadoc or one of the winners",
+            "37": "Idk, ask Zadoc ***HA REUSED TEXT***",
+            "38": "Finish your level before looking at other levels",
+            "alpha": "https://cdn.discordapp.com/attachments/335895133805477889/436597671969554432/unknown.png",
+            "beta": "IDK, go bother <@307133814834987008>",
+            "gamma": "You once used this technique and now you will have to use it again",
+            "delta": "https://media.discordapp.net/attachments/497051392695861259/506424649262039040/image.gif",
+            "epsilon": "Clue",
+            "answer": "https://cdn.discordapp.com/attachments/365038956783599617/367011950858731541/Oshit.mp3",
+            "a": "A hint once hated now your best friend :) \n ```sudo ❏```"
 
-        await self.bot.say("The clue is in the description of /r/gamelevel2, which is Who?")
-
-    @commands.command()
-    async def level2(self):
-        """Clue Level 2"""
-
-        await self.bot.say("The clue is in the description of /r/gamelevel3. | What are you supposed to do on Reddit?")
-
-    @commands.command()
-    async def level3(self):
-        """Clue Level 3"""
-
-        await self.bot.say("The clue is in the description of /r/gamelevel4. The clue is We are PRO CSS! | Use old.reddit.com")
-
-    @commands.command()
-    async def level4(self):
-        """Clue Level 4"""
-
-        await self.bot.say("Make sure you have a QWERTY keyboard")
-
-    @commands.command()
-    async def level5(self):
-        """Clue Level 5"""
-
-        await self.bot.say("Pay close attention to the Reddit sidebar, and remember: two eyes.")
-
-    @commands.command()
-    async def level6(self):
-        """Clue Level 6"""
-
-        await self.bot.say("The clue is in the sidebar. It is mod vanity. | a spoiler tag in the post. Super major clue")
-
-    @commands.command()
-    async def level7(self):
-        """Clue Level 7"""
-
-        await self.bot.say("A site set up specifically for this level... hosted by the burning heavens.")
-
-    @commands.command()
-    async def level8(self):
-        """Clue Level 8"""
-
-        await self.bot.say("The most special things are out of reach sometimes.")
-
-    @commands.command()
-    async def level9(self):
-        """Clue Level 9"""
-
-        await self.bot.say(" Clue is in the title. I can't see you. | But first let ...........")
-
-    @commands.command()
-    async def level10(self):
-        """Clue Level 10"""
-
-        await self.bot.say("Clue is the header image. There are also clues in the sidebar and blatantly in CSS.")
+        }
 
     @commands.command()
-    async def level11(self):
-        """Clue Level 11"""
+    async def level(self, ctx, num):
+        if num in self.bot.clues:
+            await self.bot.say(self.bot.clues[num])
+        else:
+            await self.bot.say("Sorry that is not a level")
 
-        await self.bot.say("While the effect is best if you have RES, you could rely on a previous trick to find the answer.")
-
-    @commands.command()
-    async def level12(self):
-        """Clue Level 12"""
-
-        await self.bot.say("It's Shane is the clue")
-
-    @commands.command()
-    async def level13(self):
-        """Clue Level 13"""
-
-        await self.bot.say("So far you've been advancing through time... Now you need to roll it back to 0.")
-
-    @commands.command()
-    async def levelx(self):
-        """Clue Level ?"""
-
-        await self.bot.say("It's a Übchi. The keyword is game.")
-
-    @commands.command()
-    async def level14(self):
-        """Clue Level 14"""
-
-        await self.bot.say("Review the first few levels. You may find there's a level you haven't been to yet.")
-        
-
-    @commands.command()
-    async def level15(self):
-        """Clue Level 15"""
-
-        await self.bot.say("The date is one number off.")
-
-    @commands.command()
-    async def levelxx(self):
-        """Clue Level 1?"""
-
-        await self.bot.say("The clue is in the description of level 15.")
-
-    @commands.command()
-    async def level16(self):
-        """Clue Level 16"""
-
-        await self.bot.say("53 6f 20 79 6f 75 20 74 68 6f 75 67 68 74 20 74 68 61 74 20 79 6f 75 20 77 6f 75 6c 64 20 66 69 6e 64 20 74 68 65 20 61 6e 73 77 65 72 20 62 79 20 64 65 63 6f 64 69 6e 67 20 74 68 69 73 20 74 65 78 74 20 62 75 74 20 74 68 61 74 20 69 73 20 62 75 6c 6c 73 68 69 74 2c 20 67 6f 20 62 61 63 6b 20 74 6f 20 74 68 65 20 73 75 62 72 65 64 64 69 74 20 61 6e 64 20 6c 6f 6f 6b 20 44 45 45 50 20 69 6e 74 6f 20 74 68 65 20 70 69 63 74 75 72 65 ")
-
-    @commands.command()
-    async def level17(self):
-        """Clue Level 17"""
-
-        await self.bot.say("Why might somebody use a program with an onion icon?")
-
-    @commands.command()
-    async def level18(self):
-        """Clue Level 18"""
-
-        await self.bot.say("hidden spoiler")
-
-    @commands.command()
-    async def level19(self):
-        """Clue Level 19"""
-
-        await self.bot.say("Secret Sauce")
-
-    @commands.command()
-    async def levelxxx(self):
-        """Clue Level I6"""
-
-        await self.bot.say("A gif is stickied - Look at it :thinking:")
-
-    @commands.command()
-    async def level20(self):
-        """Clue Level 20"""
-
-        await self.bot.say("The clue is, Hard to h**ear** with a picture of an empty vase.")
-
-    @commands.command()
-    async def level21(self):
-        """Clue Level 21"""
-
-        await self.bot.say("I soametims do typos :thinking:")
-
-    @commands.command()
-    async def level22(self):
-        """Clue Level 22"""
-
-        await self.bot.say("One way to solve is to find /r/FutureGaming")
-
-    @commands.command()
-    async def level23(self):
-        """Clue Level 23"""
-
-        await self.bot.say("The clue is, who am I (zadoc) not?")
-
-    @commands.command()
-    async def level24(self):
-        """Clue Level 24"""
-
-        await self.bot.say("The clue is american wow.")
-
-    @commands.command()
-    async def level25(self):
-        """Clue Level 25"""
-
-        await self.bot.say("The clue is a reference to our Discord. Just ask the bot for the answer!")
-
-    @commands.command()
-    async def level26(self):
-        """Clue Level 26"""
-
-        await self.bot.say("The clue is a set of multicolored [!] in the sidebar")
-
-    @commands.command()
-    async def level27(self):
-        """Clue Level 27"""
-
-        await self.bot.say("The clue is, I cant believe we never used this one before. Also, in the description it says, naw.")
-
-    @commands.command()
-    async def level272(self):
-        """Clue Level 272"""
-
-        await self.bot.say("There are four RUE's, all the E's are red, and there's a pair of dice.")
-
-    @commands.command()
-    async def level28(self):
-        """Clue Level 28"""
-
-        await self.bot.say("Clue = Wat dis?")
-
-    @commands.command()
-    async def level29(self):
-        """Clue Level 29"""
-
-        await self.bot.say("The clue is: X - α4 = Y. Solve for Y. I give them that according to Zadoc")
-
-    @commands.command()
-    async def level30(self):
-        """Clue Level 30"""
-
-        await self.bot.say("What happened in 1997?")
-
-    @commands.command()
-    async def level31(self):
-        """Clue"""
-
-        await self.bot.say("Give no hints except 1, is our order! Sorry ;P")
-
-    @commands.command()
-    async def level32(self):
-        """Clue Level 32"""
-
-        await self.bot.say("Do the things my creator doesnt do often")
-
-    @commands.command()
-    async def level33(self):
-        """Clue Level 33"""
-
-        await self.bot.say("They have to decode the sound")
-
-    @commands.command()
-    async def level34(self):
-        """Clue Level 34"""
-
-        await self.bot.say("90's kids will remember text like this")
-
-    @commands.command()
-    async def level35(self):
-        """Clue Level 35"""
-
-        await self.bot.say("Ask a mod for hints to the numbers, this one is impossible for to answer as a bot - Be angry at Tom for not giving me AI")
-
-    @commands.command()
-    async def level36(self):
-        """Clue Level 36"""
-
-        await self.bot.say("Idk, ask Zadoc or one of the winners")
-
-    @commands.command()
-    async def level37(self):
-        """Clue Level 37"""
-
-        await self.bot.say("Idk, ask Zadoc ***HA REUSED TEXT***")
-
-    @commands.command()
-    async def level38(self):
-        """Clue Level 38"""
-
-        await self.bot.say("Finish your level before looking at other levels")
-        
-    @commands.command()
-    async def levelalpha(self):
-        """Clue Level alpha"""
-
-        await self.bot.say("https://cdn.discordapp.com/attachments/335895133805477889/436597671969554432/unknown.png")
-        
-    @commands.command()
-    async def levelbeta(self):
-        """Clue Level beta"""
-        
-        await self.bot.say("IDK, go bother <@307133814834987008>")
-        
-    @commands.command()
-    async def levelgamma(self):
-        """Clue Level gamma"""
-
-        await self.bot.say("You once used this technique and now you will have to use it again")
-        
-    @commands.command()
-    async def leveldelta(self):
-        """Clue Level delta"""
-
-        await self.bot.say("https://media.discordapp.net/attachments/497051392695861259/506424649262039040/image.gif")
-        
-    @commands.command()
-    async def levelepsilon(self):
-        """Clue Level epsilon"""
-
-        await self.bot.say("Clue")
-
-    @commands.command()
-    async def answer(self):
-        """OwO wat dis"""
-
-        await self.bot.say("https://cdn.discordapp.com/attachments/365038956783599617/367011950858731541/Oshit.mp3")
-
-    @commands.command()
-    async def levela(self):
-        """Clue LevelA"""
-
-        await self.bot.say("A hint once hated now your best friend :) \n ```sudo ❏```")    
-        
     @commands.command()
     async def help(self):
         """Help"""
-        
+
         await self.bot.say("Use the command klevel[number] to get a hint for your level.")
+
 
 def setup(bot):
     bot.remove_command("help")
